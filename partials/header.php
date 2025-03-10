e4aabd7b6b716b9f0df50cfce3ca606f4e2685af
BertheAdrien <berthe.adrien@yahoo.fr>
2025-03-10 17:58:00 +0100

    jy crois toujours pas

--- partials/header.php
+++ partials/header.php
@@ -35,7 +35,6 @@ $title = isset($title) ? $title : 'Welcome training';
     <link rel="stylesheet" href="../assets/CSS/dashboard.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="../assets/CSS/dashboard.css">
     <link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/main.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="../assets/CSS/calendar.css" rel="stylesheet">
@@ -44,7 +43,7 @@ $title = isset($title) ? $title : 'Welcome training';
 
 <?php
 $current_page = basename($_SERVER['PHP_SELF']); // Récupère le nom du fichier courant
-// Si l'utilisateur n'est pas admin, afficher la navbar générale utilisateur
+// Si l'utilisateur est étudiant
 if ($_SESSION['user_status'] === 'Student') {
 ?>
 
