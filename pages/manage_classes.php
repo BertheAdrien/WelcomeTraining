79cc3e9b995986dd52fd208a67395c0f8ac9e4cb
BertheAdrien <berthe.adrien@yahoo.fr>
2025-03-10 20:34:08 +0100

    mega test

--- pages/manage_classes.php
+++ pages/manage_classes.php
@@ -17,11 +17,11 @@ $classes = $classManager->getAllClasses();
         <h1 class="text-center mb-4">Gestion des classes</h1>
 
         <!-- Formulaire pour ajouter une classe -->
-        <h2 class="mt-4">Ajouter une nouvelle classe</h2>
+        <!-- <h2 class="mt-4">Ajouter une nouvelle classe</h2>
         <form method="POST" action="../actions/class_actions.php" class="d-flex mb-4">
             <input type="text" name="class_name" class="form-control me-2" placeholder="Nom de la nouvelle classe" required>
             <button type="submit" name="add_class" class="btn btn-primary">Ajouter</button>
-        </form>
+        </form> -->
 
         <!-- Liste des classes -->
         <h2>Classes existantes</h2>
@@ -41,10 +41,10 @@ $classes = $classManager->getAllClasses();
                             </a>
                         </td>
                         <td>
-                            <form method="POST" action="../actions/class_actions.php" style="display:inline-block;">
+                            <!-- <form method="POST" action="../actions/class_actions.php" style="display:inline-block;">
                                 <input type="hidden" name="class_id" value="<?php echo $class['idClasse']; ?>">
                                 <button type="submit" name="delete_class" class="btn btn-danger">✖</button>
-                            </form>
+                            </form> -->
                         </td>
                     </tr>
                 <?php endforeach; ?>
