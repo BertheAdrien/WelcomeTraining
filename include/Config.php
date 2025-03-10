740e5a7d2634276d0ac05b7472b7d50c24057b98
BertheAdrien <berthe.adrien@yahoo.fr>
2025-03-10 15:08:53 +0100

    marche pas

--- include/Config.php
+++ include/Config.php
@@ -7,7 +7,7 @@ class Database {
         $host = getenv('DB_HOST')?: '127.0.0.1';
         $dbname = getenv('DB_NAME')?: 'welcometraining';
         $username = getenv('DB_USER')?: 'root';
-        $password = getenv('DB_PASS')?: 'Pokemon!!72380';
+        $password = getenv('DB_PASS');
 
         try {
             $this->pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
